fb12b5ad80dc269aec5e266fff9f5cd856a95b11
iasngap <166513900+iasngap@users.noreply.github.com>
2024-05-18 08:59:13 +0800

    Minor changes

--- Market.cpp
+++ Market.cpp
@@ -2,27 +2,6 @@
 
 using namespace std;
 
-// void RateCurve::display() const {
-//     cout << "rate curve:" << name << endl;
-//     for (size_t i=0; i<tenors.size(); i++) {
-//       cout << tenors[i] << ":" << rates[i] << endl;
-//   }
-//   cout << endl;
-// }
-
-// void RateCurve::addRate(Date tenor, double rate) {
-//   //consider to check if tenor already exist
-//   if (true){
-//     tenors.push_back(tenor);
-//     rates.push_back(rate);
-//   }  
-// }
-
-// double RateCurve::getRate(Date tenor) const {
-//   //use linear interpolation to get rate
-//   return 0;
-
-// }
 
 // Implement the display function outside the class declaration
 void RateCurve::display() const {
@@ -74,3 +53,5 @@ std::istream& operator>>(std::istream& is, Market& mkt)
   is >> mkt.asOf;
   return is;
 }
+
+
